5ad562b41f0410713c8d0d4846e853d0e27e9f6a
NyDamon <damon@thecreditpros.com>
2025-08-19 12:22:37 -0400

    feat: update to modern ESLint flat config format (fixes parsing errors)

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/coverage/**',
+      '**/*.min.js',
+      '**/site/**',
+      '**/docs/site/**',
+      '**/temp/**',
+    ],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        global: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  },
+];
